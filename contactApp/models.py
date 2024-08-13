9387e0ec4d728bd58794fd97ba44de36da7ac46f
midhulakk <midhulaniyakk@gmail.com>
2024-08-13 21:43:47 +0530

    'initial'

---
+++ contactApp/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class ContactModel(models.Model):
+    name=models.CharField(max_length=100)
+    phone=models.IntegerField()
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+
